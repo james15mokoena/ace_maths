4909eb2d1e4a87e48200efca3097b65480e4ba78
james15mokoena <jamesmokoena15@yahoo.com>
2026-07-25 09:05:14 +0200

    Implementing the trigonometric functions page.

--- lib/pages/trigonometric_functions.dart
+++ lib/pages/trigonometric_functions.dart
@@ -8,7 +8,34 @@ class TrigonometricFunctionsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TopAppbar(title: "Trigonometric Functions"),
-      body: const Text("Trigonometric Functions Content"),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(5),
+            child: Align(
+              child: const Text(
+                "Study",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Align(
+              alignment: .topLeft,
+              child: const Text(
+                "1. What is a Trigonometric Function?",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 25,
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.red,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
